8e0c7fa5fc71f4abb7ede35784c3f57257308cf3
Jai <jaijai.stell@gmail.com>
2023-03-23 10:57:09 -0700

    braain hurts - completes lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// jais code
+
+function findMatching(drivers, name) {
+  let driversName = drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+  return driversName;
+}
+
+function fuzzyMatch(drivers, name) {
+  let driversName = drivers.filter(function (driver) {
+    return driver.slice(0, 2).toLowerCase() === name.toLowerCase();
+  });
+  return driversName;
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
+//arrow function! I seriously just dont get it
